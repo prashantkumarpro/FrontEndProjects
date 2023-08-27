99d7ae991cdd36658e8ef0a6c1df75018dab14b3
prashantkumarpro <programmerkrprashant@gmail.com>
2023-08-27 20:33:55 +0530

    insta page liked Js code

---
+++ projectWithDOM/project2/script.js
@@ -0,0 +1,7 @@
+let postImg = document.querySelector('#post-img');
+let ftrLove = document.querySelector('#ftr-love');
+let likedLove = document.querySelector('#liked-love');
+
+postImg.addEventListener("dblclick", function(e){
+ console.log('helloosjfslf')
+})
