cd501f810a9b55b95b979c8b4d68fd3de772f07a
prashantkumarpro <programmerkrprashant@gmail.com>
2023-08-28 22:05:04 +0530

    insta Love Btn created

--- projectWithDOM/project2/script.js
+++ projectWithDOM/project2/script.js
@@ -1,7 +1,42 @@
-let postImg = document.querySelector('#post-img');
-let ftrLove = document.querySelector('#ftr-love');
-let likedLove = document.querySelector('#liked-love');
 
-postImg.addEventListener("dblclick", function(e){
- console.log('helloosjfslf')
+let ftrLove = document.querySelectorAll('.ftr-love');
+let likedLove = document.querySelectorAll('#liked-love');
+let instaLoveBtn = document.querySelectorAll('#solid-heart');
+
+
+
+let postImgs = document.querySelectorAll('.mid');
+
+postImgs.forEach(e => {
+
+    e.childNodes[3].addEventListener('dblclick', () => {
+        instaLoveBtn.forEach((e, key) =>{
+            e.style.opacity = 1;
+            console.log(`key1 : ${key}`)
+        })
+       
+        console.log('hello')
+
+        setTimeout(function () {
+          instaLoveBtn.forEach(e => {
+            console.log(e.tagName)
+          })
+            ftrLove.forEach((e, key) => {
+                e.style.display = "none"
+                console.log(key)
+            })
+            
+            // likedLove.style.display = "block"
+        }, 2000)
+    })
 })
+
+
+
+likedLove.forEach(e => {
+    e.addEventListener('click', () => {
+        ftrLove.style.display = "block"
+        likedLove.style.display = "none"
+    })
+})
+
