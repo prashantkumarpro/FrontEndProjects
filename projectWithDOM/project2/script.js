f2e1132a68c6b7c5d6ffeb73acd526da9f8c7023
prashantkumarpro <programmerkrprashant@gmail.com>
2023-08-29 15:58:50 +0530

    Insta Love Button Js Logic build

--- projectWithDOM/project2/script.js
+++ projectWithDOM/project2/script.js
@@ -1,42 +1,27 @@
 
-let ftrLove = document.querySelectorAll('.ftr-love');
-let likedLove = document.querySelectorAll('#liked-love');
-let instaLoveBtn = document.querySelectorAll('#solid-heart');
+
+let postContainer = document.querySelectorAll('.post-container');
 
 
+postContainer.forEach(function (elem) {
+    // console.log(elem.childNodes[5].childNodes[1].childNodes[1].childNodes[3])
+    let solidHeart = elem.childNodes[3].childNodes[1];
+    let footerLove = elem.childNodes[5].childNodes[1].childNodes[1].childNodes[1];
+    console.log(footerLove.childNodes[0])
+    let likedLove = elem.childNodes[5].childNodes[1].childNodes[1].childNodes[3];
 
-let postImgs = document.querySelectorAll('.mid');
+    elem.childNodes[3].childNodes[3].addEventListener('dblclick', function () {
+        solidHeart.style.opacity = 1;
+        setTimeout(() => {
+            solidHeart.style.opacity = 0;
+            footerLove.style.display = 'none'
+            likedLove.style.display = 'block'
+        }, 3000);
 
-postImgs.forEach(e => {
-
-    e.childNodes[3].addEventListener('dblclick', () => {
-        instaLoveBtn.forEach((e, key) =>{
-            e.style.opacity = 1;
-            console.log(`key1 : ${key}`)
+        likedLove.addEventListener('click', function () {
+            footerLove.style.display = 'block'
+            likedLove.style.display = 'none'
         })
-       
-        console.log('hello')
-
-        setTimeout(function () {
-          instaLoveBtn.forEach(e => {
-            console.log(e.tagName)
-          })
-            ftrLove.forEach((e, key) => {
-                e.style.display = "none"
-                console.log(key)
-            })
-            
-            // likedLove.style.display = "block"
-        }, 2000)
-    })
-})
-
-
-
-likedLove.forEach(e => {
-    e.addEventListener('click', () => {
-        ftrLove.style.display = "block"
-        likedLove.style.display = "none"
     })
 })
 
